28ca6a1522258334a802b79effcd108894c21cfb
mgritsch04 <Gritsch.Maximilian@sus.htl-grieskirchen.at>
2021-10-19 14:38:22 +0200

    4.bsp

---
+++ src/net/htlgkr/mgritsch194/Main.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.htlgkr.mgritsch194;
+
+import java.util.stream.IntStream;
+
+/**
+ *
+ * @author maxim
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        final int result = IntStream.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10).filter(i -> i % 2 != 0).map(i -> i * i).reduce((a, b) -> a + b).getAsInt();
+    }
+}
