24a299f3cb666af6cf53a0a0c133fd837b59bc2a
mgritsch04 <Gritsch.Maximilian@sus.htl-grieskirchen.at>
2021-10-19 14:10:56 +0200

    3. Bsp

--- src/net/htlgkr/mgritsch192/Main.java
+++ src/net/htlgkr/mgritsch192/Main.java
@@ -28,7 +28,45 @@ public class Main {
 
         System.out.println("findWeaponWithLowestDamage:");
         System.out.println(s.findWeaponWithLowestDamage(s.list).toString());
-
         System.out.println("");
+
+        System.out.println("findWeaponWithHighestStrength");
+        System.out.println(s.findWeaponWithHighestStrength(s.list).toString());
+        System.out.println("");
+
+        System.out.println("collectMissileWeapons:");
+        System.out.println(s.collectMissileWeapons(s.list));
+        System.out.println("");
+
+        System.out.println("findWeaponWithLongstNKame:");
+        System.out.println(s.findWeaponWithLongestName(s.list).toString());
+        System.out.println("");
+
+        System.out.println("toNameList:");
+        System.out.println(s.toNameList(s.list));
+        System.out.println("");
+
+        System.out.println("toSpeedArray:");
+        int[] a = s.toSpeedArray(s.list);
+        for (int i : a) {
+            System.out.print(i + ", ");
+        }
+        System.out.println("");
+        System.out.println("");
+
+        System.out.println("sumUpValues:");
+        System.out.println(s.sumUpValues(s.list));
+        System.out.println("");
+
+        System.out.println("sumUpHashCodes:");
+        System.out.println(s.sumUpHashCodes(s.list));
+        System.out.println("");
+
+        System.out.println("removeDuplicates");
+        System.out.println(s.removeDuplicates(s.list));
+        System.out.println("");
+
+        System.out.println("increaseValuesByTenPercent");
+        System.out.println(s.list);
     }
 }
