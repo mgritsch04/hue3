3ea82914519d8fae92b6b5728fd6476a9d6bddf5
mgritsch04 <Gritsch.Maximilian@sus.htl-grieskirchen.at>
2021-10-19 14:27:34 +0200

    4. Bsp

--- src/net/htlgkr/mgritsch193/Main.java
+++ src/net/htlgkr/mgritsch193/Main.java
@@ -5,6 +5,8 @@
  */
 package net.htlgkr.mgritsch193;
 
+import java.util.function.Predicate;
+
 /**
  *
  * @author maxim
@@ -12,6 +14,10 @@ package net.htlgkr.mgritsch193;
 public class Main {
 
     public static void main(String[] args) {
+        final Predicate<Integer> isEven = (t) -> {
+            return (t % 2 == 0);
+        };
+        System.out.println("Die Zahl 6 ist Gerade: " + isEven);
 
     }
 }
